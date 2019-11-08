da9687944e40737f1ee0ef534cb6e8363a5664b3
Andrew614 <azhang213@gmail.com>
2019-11-08 00:28:47 -0500

    add json-server and axios dependencies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import Person from './components/Person'
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form'
 import FilterSearch from './components/FilterSearch'
 import People from './components/People'
+import axios from 'axios'
 
 const App = () => {
-  const [people, setPeople] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [people, setPeople] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredName, setFilteredName] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPeople(response.data)
+    })
+  }, [])
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
